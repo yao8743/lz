35a7bf495db955f3bff3cbfddec9deda428c0bb5
little2 <agile@little2.net>
2026-04-30 04:16:01 +0800

    update

--- man_config.py
+++ man_config.py
@@ -1,4 +1,22 @@
+import json
+import os
+
 from dotenv import load_dotenv
+
 load_dotenv(dotenv_path='.man.env')
+
+config: dict = {}
+
+try:
+	configuration_json = json.loads(os.getenv('CONFIGURATION', '') or '{}')
+	if isinstance(configuration_json, dict):
+		config.update(configuration_json)
+except Exception as error:
+	print(f"⚠️ 無法解析 CONFIGURATION：{error}")
+
+
+API_ID = int(config.get('api_id', os.getenv('API_ID', 0)))
+API_HASH = config.get('api_hash', os.getenv('API_HASH', ''))
+SESSION_STRING = config.get('session_string', os.getenv('SESSION_STRING', ''))
 
 
